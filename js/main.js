b8fcff9707cef02feec46658e60db8ce2b618bdc
Kyle-Jackson <kylejackson@clare-pc.gateway.2wire.net>
2016-03-18 13:43:38 -0400

    Not important.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-var $input = $('#list-input');
-var $list = $('.todo');
-
-$('form').on('submit', function (e) {
-  var $li = $('<li>');
-
-  e.preventDefault();
-  $li.html($input.val());
-  $li.val('');
-  $list.append($li);
-});
-
-$('ul').on('click', 'li', function () {
-  $(this).toggleClass('crossOut');
-});
+ಥ_ಥ
